742ad0043b8ff8a5268c1873302857f9528dffe2
CT-66 <css.com@protonmail.com>
2021-11-12 13:33:24 +0530

    12 Nov, 2021 | 01:33 PM

--- zsh/plugins/install-plugins.sh
+++ zsh/plugins/install-plugins.sh
@@ -5,12 +5,16 @@
 ln -s ~/Dotfiles/zsh/.zshenv ~/.zshenv
 
 plugins=(
+    "https://github.com/romkatv/powerlevel10k"
     "https://github.com/zsh-users/zsh-autosuggestions"
     "https://github.com/zsh-users/zsh-syntax-highlighting"
     "https://github.com/hlissner/zsh-autopair"
 )
 
+cd ~/Dotfiles/zsh/plugins
+
 for plugin in "${plugins[@]}"; do
     # git clone $plugin ~/Dotfiles/zsh/plugins
-    git clone $plugin ~/.config/zsh/plugins
+    # git clone $plugin ~/.config/zsh/plugins
+    git clone $plugin
 done
