4ef807c5821312cb2e1d45b71513bedbfec910bf
CT-66 <css.com@protonmail.com>
2021-11-25 21:26:30 +0530

    25 Nov, 2021 | 09:26 PM

--- polybar/scripts/reddit.sh
+++ polybar/scripts/reddit.sh
@@ -5,7 +5,7 @@ export $(cat ~/Dotfiles/polybar/scripts/.env | xargs)
 notifications=$(curl -sf --user-agent "$USERAGENT" "$URL" | jq '.["data"]["children"] | length')
 
 if [ -n "$notifications" ] && [ "$notifications" -gt 0 ]; then
-    echo "%{F#ff4500} $notifications"
+    echo "%{F#ff4500}  $notifications"
 else
     echo ""
 fi
