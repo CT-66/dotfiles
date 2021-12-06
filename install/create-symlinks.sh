41812d4bdcc6b7492ade4318f59dcbb85cdf37a2
CT-66 <css.com@protonmail.com>
2021-12-06 16:37:57 +0530

    06 Dec, 2021 | 04:37 PM

--- install/create-symlinks.sh
+++ install/create-symlinks.sh
@@ -21,6 +21,7 @@ dirs=(
     zsh
     ranger
     bat
+    doom
 )
 
 if [ -d ~/.config ]; then
@@ -38,7 +39,6 @@ echo "Done"
 ln -s ~/Dotfiles/xinitrc ~/.xinitrc
 ln -s ~/Dotfiles/Xmodmap ~/.Xmodmap
 ln -s ~/Dotfiles/Xresources ~/.Xresources
-ln -s ~/Dotfiles/.emacs.d ~/.emacs.d
 ln -s ~/Dotfiles/.doom.d ~/.doom.d
 sudo mv /usr/share/icons/default/index.theme /usr/share/icons/default/index.theme.bak
 sudo ln -s ~/Dotfiles/.icons/default/index.theme /usr/share/icons/default/index.theme
