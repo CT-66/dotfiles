e5576169b0f1ee7dff969c4293eaeeb407493723
CT-66 <css.com@protonmail.com>
2021-12-22 14:34:33 +0530

    22 Dec, 2021 | 02:34 PM

--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@
 $HOME/Dotfiles/polybar/launch.sh &
 pkill sxhkd; sxhkd &
 #  xrandr --output Virtual1 --mode 1920x1080
-feh --bg-scale ~/Pictures/Wallpapers/065.jpg &
+# feh --bg-scale ~/Pictures/Wallpapers/065.jpg &
+feh --bg-scale ~/Pictures/Wallpapers/066.jpg &
 export $(dbus-launch) && dunst &
 $HOME/Scripts/dotfiles &
 # flashfocus
