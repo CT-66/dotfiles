2197f2af77548ee4a4b4c3fa320010d91fca55a3
CT-66 <css.com@protonmail.com>
2022-01-05 13:33:44 +0530

    05 Jan, 2022 | 01:33 PM

--- qtile/config.py
+++ qtile/config.py
@@ -3,6 +3,8 @@ from typing import List  # noqa: F401
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
+from libqtile import hook
+
 
 # from libqtile.utils import guess_terminal
 
