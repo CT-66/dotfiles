d3719c989c0126fcb3ae9aef45bb628bb172e13d
CT-66 <css.com@protonmail.com>
2022-01-07 20:53:07 +0530

    07 Jan, 2022 | 08:53 PM

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-xrdb -merge "~/Dotfiles/X11/Xresources" &
-xmodmap "~/Dotfiles/X11/Xmodmap" &
-
 $HOME/Dotfiles/polybar/launch.sh &
 pkill sxhkd; sxhkd &
 #  xrandr --output Virtual1 --mode 1920x1080
