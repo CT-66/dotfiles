0dae491764cca263de904ac2d9ce3081df7a5e08
CT-66 <css.com@protonmail.com>
2022-01-10 16:11:55 +0530

    10 Jan, 2022 | 04:11 PM

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-$HOME/Dotfiles/polybar/launch.sh &
 pkill sxhkd; sxhkd &
 #  xrandr --output Virtual1 --mode 1920x1080
 # feh --bg-scale ~/Pictures/Wallpapers/066.jpg &
@@ -10,7 +9,6 @@ pkill sxhkd; sxhkd &
 feh --bg-scale ~/Pictures/Wallpapers/011.jpg &
 # feh --bg-scale ~/Pictures/Wallpapers/049.jpg &
 export $(dbus-launch) && dunst &
-$HOME/Scripts/dotfiles &
 numlockx &
 pkill greenclip; greenclip daemon &
 pkill nm-applet; nm-applet &
@@ -21,3 +19,5 @@ pkill picom; picom -b &
 # picom-jonaburg
 # pkill picom; picom -f --experimental-backend -b &
 pkill flashfocus; flashfocus &
+$HOME/Dotfiles/polybar/launch.sh &
+$HOME/Scripts/dotfiles &
