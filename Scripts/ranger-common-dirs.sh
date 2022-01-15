a2c9be228c6cfc4750b773353aa049c96908aebb
CT-66 <css.com@protonmail.com>
2022-01-16 03:22:41 +0530

    16 Jan, 2022 | 03:22 AM

---
+++ Scripts/ranger-common-dirs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERNAME=$(whoami)
+
+dirs=("$HOME/.config"
+    "$HOME/Books"
+    "$HOME/Code"
+    "$HOME/Desktop"
+    "..."
+)
+
+str=""
+for dir in "${dirs[@]}"; do
+    str+=$dir
+    str+=" "
+done
+
+ranger_dir="$(echo $str | sed "s/\/home\/$USERNAME/\~/" | rofi -dmenu -sep " " -p "")"
+
+st -e "ranger \"$ranger_dir\""
