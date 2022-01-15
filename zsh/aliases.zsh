011d65b0f853f9f5524c7513204676e5a144b9f6
CT-66 <css.com@protonmail.com>
2022-01-15 14:14:16 +0530

    15 Jan, 2022 | 02:14 PM

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias cmatrix="cmatrix -bs -C blue"
 # alias .date='date +"%d %b, %Y | %I:%M %p"'
  alias .date='date +"%a %d %b | %I:%M %p"'
 alias grep="grep -ina --color=auto"
-alias find='find -iname'
+alias find='fd'
 alias .history='nvim ~/.cache/zsh/history'
 alias g=git
 alias p='sudo pacman'
