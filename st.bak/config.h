2d55d63cb84cf5d95d0170573cbbc6a031a4cdd1
CT-66 <css.com@protonmail.com>
2022-01-16 03:54:56 +0530

    16 Jan, 2022 | 03:54 AM

--- st.bak/config.h
+++ st.bak/config.h
@@ -101,7 +101,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 float alphaOffset = 0.0;
 float alphaUnfocus;
 
@@ -267,8 +267,8 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
-	/* { MODKEY,		XK_s,		changealpha,	{.f = -0.05} }, */
-	/* { MODKEY,		XK_a,		changealpha,	{.f = +0.05} }, */
+        { MODKEY,		XK_q,		changealpha,	{.f = -0.05} }, 
+	{ MODKEY,		XK_w,		changealpha,	{.f = +0.05} }, 
 	/* { TERMMOD,              XK_Up,          zoom,           {.f = +1} }, */
 	/* { TERMMOD,              XK_Down,        zoom,           {.f = -1} }, */
 	/* { TERMMOD,              XK_K,           zoom,           {.f = +1} }, */
