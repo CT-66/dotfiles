69a2165f7324dc1bb73859d9cfcef26b1e890f9e
CT-66 <css.com@protonmail.com>
2022-02-02 20:28:17 +0530

    02 Feb, 2022 | 08:28 PM

--- polybar/scripts/qtile.py
+++ polybar/scripts/qtile.py
@@ -1,7 +1,24 @@
 #!/bin/python3
+from libqtile.command.client import InteractiveCommandClient
+from libqtile.lazy import lazy
+import sys
+
+c = InteractiveCommandClient()
+args = sys.argv
+
+if len(args) > 1:
+    arg = args[1]
+else:
+    arg = ""
+
 try:
-    from libqtile.command.client import InteractiveCommandClient
-    c = InteractiveCommandClient()
     print(c.layout.info().get("name"))
 except:
     print("")
+
+if arg == "next":
+    lazy.next_layout()
+elif arg == "prev":
+    lazy.prev_layout()
+else:
+    pass
