9c1268dfb5d2dd10a242b20a49195094c41ab859
CT-66 <css.com@protonmail.com>
2022-02-02 13:51:37 +0530

    02 Feb, 2022 | 01:51 PM

--- qtile/config.py
+++ qtile/config.py
@@ -67,14 +67,14 @@ for i in groups:
                 [MOD],
                 i.name,
                 lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+                # desc="Switch to group {}".format(i.name),
             ),
             # MOD1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [MOD, shift],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                # desc="Switch to & move focused window to group {}".format(i.name),
             ),
             # Or, use below if you prefer not to switch to that group.
             # # MOD1 + shift + letter of group = move focused window to group
