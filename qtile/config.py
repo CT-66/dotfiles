9f60438a90e60a6edb1e0734b0b41875f1fba945
CT-66 <css.com@protonmail.com>
2022-02-02 14:15:39 +0530

    02 Feb, 2022 | 02:15 PM

--- qtile/config.py
+++ qtile/config.py
@@ -89,7 +89,8 @@ layouts = [
         border_width = 2,
         margin = 15,
         border_on_single = True,
-        margin_on_single = 15
+        margin_on_single = 15,
+        insert_position = 1
     ),
     layout.Max(),
     # layout.MonadTall(),
