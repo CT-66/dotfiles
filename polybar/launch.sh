b21d585fc230c438961986ed1e2e98e66bf68bdb
CT-66 <css.com@protonmail.com>
2022-02-05 13:09:06 +0530

    05 Feb, 2022 | 01:09 PM

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 xdotool search --all --screen $DISPLAY --classname polybar windowkill %@
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Polybar launched..."
