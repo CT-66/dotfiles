440f2f03a2b1794261d0fb7d3ed3b1fffc1c8fe6
CT-66 <css.com@protonmail.com>
2022-02-07 23:25:47 +0530

    07 Feb, 2022 | 11:25 PM

--- polybar/scripts/py-rss.py
+++ polybar/scripts/py-rss.py
@@ -2,31 +2,47 @@
 from feedparser import parse
 import signal
 from os import system
+from random import choice
+import time
+
+urls = [
+    "https://www.makeuseof.com/feed/category/linux/",
+    "https://linuxhint.com/feed/",
+    "https://linuxize.com/index.xml",
+    "https://itsfoss.com/feed/",
+    "https://www.fossmint.com/feed/",
+    "https://dev.to/feed/tag/linux",
+    "https://dev.to/feed/tag/archlinux",
+]
 
 
 def main():
-    # url = "https://www.archlinux.org/feeds/news"
-    url = "https://www.makeuseof.com/feed/category/linux/"
+    url = choice(urls)
     entries = 1
 
     for x in parse(url).entries[:entries]:
-        print(f"索 {x.title}")
+        global title
         global link
+        title = x.title
         link = x.link
-
-
-# exec qutebrowser "link"
+        print(f"索 {title}")
 
 
 def handler(signum, frame):
     if signum == signal.SIGUSR1:
         system(f'qutebrowser "{link}"')
+    # change feed
+    elif signum == signal.SIGUSR2:
+        main()
 
 
 if __name__ == "__main__":
     try:
         main()
         while True:
+            print(f"索 {title}")
+            time.sleep(1)
             signal.signal(signal.SIGUSR1, handler)
+            signal.signal(signal.SIGUSR2, handler)
     except:
         print("索 Error")
