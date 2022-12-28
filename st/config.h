94fd8a66ecad2130229bb413d72e9a46653ff635
CT-66 <css.com@protonmail.com>
2022-12-28 09:44:13 +0530

    28 Dec, 2022 | 09:44 AM

--- st/config.h
+++ st/config.h
@@ -265,8 +265,8 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
 	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
     // vim navigation (alt+d, alt+u, alt+j, alt+k)
-	/* { MODKEY,               XK_k,           kscrollup,      {.i =  1} }, */
-	/* { MODKEY,               XK_j,           kscrolldown,    {.i =  1} }, */
+	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_Up,          kscrollup,      {.i =  1} },
 	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
