b4e0e3e622e7eeadd9bbc563e472fa922d09c3ab
CT-66 <css.com@protonmail.com>
2024-06-29 21:04:58 +0530

    29 Jun, 2024 | 09:04 PM

--- Scripts/looking-glass.sh
+++ Scripts/looking-glass.sh
@@ -3,4 +3,7 @@ pkill xremap
 looking-glass-client -s -F -S -m "KEY_RIGHTCTRL"
 # xremap --watch /home/arch/.config/xremap/config.yml & disown
 # xremap --watch /home/arch/.config/xremap/config.yml && echo "Wait!\n" && sleep 3 && echo "Done" & disown
+echo "Wait..."
+sleep 5
+echo "Done."
 nohup xremap --watch /home/arch/.config/xremap/config.yml &
