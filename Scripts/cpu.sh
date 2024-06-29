d88e5e48a84a1019e086d406fd2767696e24463b
CT-66 <css.com@protonmail.com>
2024-06-29 16:43:12 +0530

    29 Jun, 2024 | 04:43 PM

--- Scripts/cpu.sh
+++ Scripts/cpu.sh
@@ -3,4 +3,4 @@
 # cpu=$(mpstat 1 1 | awk '$13 ~ /[0-9.]+/ {print 100 - $13"%"}')
 cpu=$(mpstat 1 1 | awk '$13 ~ /[0-9.]+/ {usage = 100 - $13; if (usage == int(usage)) printf "%.2f%%\n", usage; else printf "%.2f%%\n", usage}')
 
-echo " CPU $cpu "
+echo " $cpu "
--- Scripts/looking-glass.sh
+++ Scripts/looking-glass.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pkill xremap
 looking-glass-client -s -F -S -m "KEY_RIGHTCTRL"
-xremap --watch /home/arch/.config/xremap/config.yml & disown
+# xremap --watch /home/arch/.config/xremap/config.yml & disown
 # xremap --watch /home/arch/.config/xremap/config.yml && echo "Wait!\n" && sleep 3 && echo "Done" & disown
+nohup xremap --watch /home/arch/.config/xremap/config.yml &
